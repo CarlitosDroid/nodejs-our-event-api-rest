c7ed8d330cddb536b1673ffa52c41b4bab68bbe8
DavidTenorioRivas <david.tenorio.rivas@gmail.com>
2017-02-28 22:54:11 -0500

    wip(script-our-event: Add table placelocation)

--- mysql/script-our-event.sql
+++ mysql/script-our-event.sql
@@ -30,3 +30,66 @@ DELIMITER ;
 CALL sp_GetEmployee();
 /******************************************************************/
 
+
+
+
+CREATE TABLE placelocation (
+  IdPlace   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  Name         VARCHAR(40)                    NULL,
+  Latitud      INT                            NULL,
+  Longitud     INT                            NULL,
+  CreationDate DATETIME                       NULL
+)
+  ENGINE = INNODB;
+
+/* INSERT DATA */
+INSERT INTO EMPLOYEE (FirstName, LastName, Phone, Address, City, FullName, Email, Pin, CreationDate, EmployeeCode)
+VALUES ('SYSADMIN', 'SYSADMIN', 945214775, 'Av. Alfonso Ugarte', 'Lima', 'SYSADMIN SYSADMIN', 'sysadmin@gmail.com',
+        1, '2011-12-18 13:17:17', 'SYSADMIN');
+
+INSERT INTO placelocation(Name,Latitud,Longitud,CreationDate)
+VALUES('Metro Breña',152503,-124201,'2011-12-18 13:17:17');
+
+INSERT INTO placelocation(Name,Latitud,Longitud,CreationDate)
+VALUES('MegaPlaza',15103,-1214534201,'2011-12-18 13:17:17');
+
+INSERT INTO placelocation(Name,Latitud,Longitud,CreationDate)
+VALUES('StarBucks',1514503,-12424201,'2011-12-18 13:17:17');
+
+INSERT INTO placelocation(Name,Latitud,Longitud,CreationDate)
+VALUES('LandPark',1514503,-124645201,'2011-12-18 13:17:17');
+
+DROP PROCEDURE IF EXISTS sp_GetEmployee;
+DELIMITER //
+CREATE PROCEDURE sp_GetEmployee()
+  BEGIN
+    SELECT * FROM EMPLOYEE;
+  END //
+DELIMITER ;
+
+
+
+/**Drop StoreProcedure**/
+CALL sp_GetEmployee();
+/******************************************************************/
+DROP PROCEDURE IF EXISTS sp_GetPlaceLocation;
+DELIMITER //
+CREATE PROCEDURE sp_GetPlaceLocation()
+  BEGIN
+    SELECT * FROM placelocation;
+  END //
+DELIMITER ;
+
+CALL sp_GetPlaceLocation();
+
+
+DROP PROCEDURE IF EXISTS sp_deletePlaceLocation;
+DELIMITER //
+CREATE PROCEDURE sp_deletePlaceLocation()
+  BEGIN
+    DELETE FROM  placelocation;
+    WHERE Name = 'MegaPlaza';
+END //
+DELIMITER ;
+
+CALL sp_deletePlaceLocation();
