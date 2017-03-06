d43d6d60aa834713a938a12a1a692e10798e760d
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-03-05 22:05:45 -0500

    this branch contains project with docker

--- sample-service/api/routes/employee.js
+++ sample-service/api/routes/employee.js
@@ -1,23 +1,27 @@
+/**
+ * Created by Carlos Leonardo Camilo Vargas HUuamán on 12/3/16.
+ */
+
 var express = require('express');
 var router = express.Router();
-var DataBaseHandler = require("../config/DataBaseHandler");
+var DataBaseHandler = require("./DataBaseHandler");
 var dataBaseHandler = new DataBaseHandler();
 
 var connection = dataBaseHandler.createConnection();
 
 router.get('/', function (req, res, next) {
-    connection.query('CALL sp_GetPlaceLocation();', function (error, result, fields) {
+    connection.query('CALL sp_GetEmployee();', function (error, result, fields) {
         if (error) throw error;
 
         if(result[0].length== 0){
             res.status(404).send({
                 status : "ERROR",
-                message: "No existe lugar en Base de Datos :D"
+                message: "No existe usuario en Base de Datos"
             });
         }else{
             res.status(202).send({
                 status : "SUCCESS",
-                message: "Place was found",
+                message: "User was found",
                 data : result[0]
             });
         }
--- sample-service/api/routes/placelocation.js
+++ sample-service/api/routes/placelocation.js
@@ -1,27 +1,23 @@
-/**
- * Created by Carlos Leonardo Camilo Vargas HUuamán on 12/3/16.
- */
-
 var express = require('express');
 var router = express.Router();
-var DataBaseHandler = require("../config/DataBaseHandler");
+var DataBaseHandler = require("./DataBaseHandler");
 var dataBaseHandler = new DataBaseHandler();
 
 var connection = dataBaseHandler.createConnection();
 
 router.get('/', function (req, res, next) {
-    connection.query('CALL sp_GetEmployee();', function (error, result, fields) {
+    connection.query('CALL sp_GetPlaceLocation();', function (error, result, fields) {
         if (error) throw error;
 
         if(result[0].length== 0){
             res.status(404).send({
                 status : "ERROR",
-                message: "No existe usuario en Base de Datos"
+                message: "No existe lugar en Base de Datos :D"
             });
         }else{
             res.status(202).send({
                 status : "SUCCESS",
-                message: "User was found",
+                message: "Place was found",
                 data : result[0]
             });
         }
